e118039ac5d729f7443b22452e9ee06560ec86d3
maxi297 <maxime@airbyte.io>
2023-12-07 16:21:01 -0500

    Blindly adding all the code

--- airbyte-integrations/connectors/source-stripe/setup.py
+++ airbyte-integrations/connectors/source-stripe/setup.py
@@ -5,7 +5,8 @@
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = ["airbyte-cdk==0.53.6", "stripe==2.56.0", "pendulum==2.1.2"]
+# FIXME update to the version where we have the testing tooling
+MAIN_REQUIREMENTS = ["airbyte-cdk==0.55.5", "stripe==2.56.0", "pendulum==2.1.2"]
 
 TEST_REQUIREMENTS = ["pytest-mock~=3.6.1", "pytest~=6.1", "requests-mock", "requests_mock~=1.8", "freezegun==1.2.2"]
 
